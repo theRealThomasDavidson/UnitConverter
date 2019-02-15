d6104fb4321ab2a37bc384f2f58db70afbb14dfa
thomas.davidson.18 <thomas.davidson.18@cnu.edu>
2019-02-15 14:19:46 -0500

    made a few changes think i'll be going in the dimensional analysis route some things seem to be working well now

--- unitCon.py
+++ unitCon.py
@@ -7,30 +7,103 @@ def convert(unitFrom = None, unitTo = None):
     :param unitTo: can be a none type placeholder or a unit to convert to
     :return: either a string of instructions or the ratio of the unit conversion
     """
+
+    ###############
+    #define your units here
+    #then add your dimension after the block below
+    ##############
     pressure={
-        "Pa":1., "bar": 100000., "at": 98066.5, "atm": 101325., "Torr": 133.322, "pfin": 6894.76
+        "Pa": 1., "bar": 100000., "at": 98066.5, "atm": 101325., "Torr": 133.322, "pfin": 6894.76
     }
-    flowRateVol={"Lps": 1., "cubftpmin": 2.11888, "cubmps": 0.001, "galps": 0.264172, "igillphr":25340.4
+    flowRateVol={
+        "Lps": 1., "cubftpmin": 2.11888, "cubmps": 0.001, "galps": 0.264172, "igillphr":25340.4
+    }
+    flowRateMass={
 
+        "kgps": 1., "lbps": 0.453592, "gphr": (2.77778*10**(-7)),"gpm":(1.66667*10**(-5)), "gps": 0.001, "gpms":1.,
+        "kgpms": 1000., "lbphr":12599.8,
+    }
+    length={
+        "m": 1.,"in":.00254
     }
 
+
+    ###############
+    #Add your dimensions here
+    ##############
     dimensions={
-        "pressure": pressure, "Volumetric Flow Rate":flowRateVol,
+        "Pressure": pressure, "Volumetric Flow Rate":flowRateVol,"Mass Flow Rate": flowRateMass, "Length":length
     }
-    for dim in dimensions:
-        if unitFrom in dimensions[dim] and unitTo in dimensions[dim]:
-            return dimensions[dim][unitTo]/dimensions[dim][unitFrom]
-    if unitFrom == None:
-        string="These are the available dimension types: "
-        for key in dimensions:
-            string= string + key+", "
 
-        return string[:-2]+"."
-    if unitFrom in dimensions:
-        string="These are the available unit types: "
-        for key in dimensions[unitFrom]:
-            string= string + key+", "
+    #TODO: handle dimensional analysis
 
-        return string[:-2]+"."
+    #TODO: handle prefixes for metric
+
+
+
+
     return "Dimension type not available or are not printed right. Try using convert([name of dimension type]) " \
            "to find the names of available units."
+
+def parseUnit(toParse):
+    """
+    this function should give me a unit in its most basic form
+    :param toParse:
+    :return: a tuple of the form (list, dbl) where the first a list of units and the ops that are preformed on them in it's base state, and the second is the mutiplier for that unit.
+    """
+    retList=[]
+    prefix = {
+        "Y": float(10 ** (24)), "Z": float(10 ** (21)), "E": float(10 ** (18)), "P": float(10 ** (15)),
+        "T": float(10 ** (12)),
+        "G": float(10 ** (9)), "M": float(10 ** (6)), "k": float(10 ** (3)), "h": float(10 ** (2)),
+        "da": float(10 ** (1)),
+        "d": float(10 ** (-1)), "c": float(10 ** (-2)), "m": float(10 ** (-3)), "u": float(10 ** (-6)),
+        "n": float(10 ** (-9)),
+        "p": float(10 ** (-12)), "f": float(10 ** (-15)), "a": float(10 ** (-18)), "z": float(10 ** (-21)),
+        "y": float(10 ** (-24)),
+    }
+    functs = {
+        "/": "div", "*": "mul", "^": "exp"
+    }
+
+
+    lookForPref = 42
+    unit = ""
+
+    for char in toParse:
+        if len(retList)>0:
+            if retList[-1]=="^":
+                if char=="-":
+                    for ii in len(retList):
+                        if retList[-ii] =="/":
+                            retList[-ii]="*"
+                            break
+                        if retList[-ii] =="*":
+                            retList[-ii]="/"
+                else:
+                    retList[-1]="^"+char
+                continue
+
+        if char in prefix and lookForPref:
+            print char, prefix[char]
+            lookForPref=0
+            if len(unit)!=0:
+                retList.append(unit)
+                unit=""
+        elif char in functs:
+            print char, functs[char]
+            lookForPref=42
+            if len(unit)!=0:
+                retList.append(unit)
+                unit=""
+
+            retList.append(char)
+        else:
+            unit=unit+char
+            print unit
+    if len(unit) != 0:
+        retList.append(unit)
+        unit = ""
+    return (retList, 0)
+
+print parseUnit("nm*uin^2/Ys")
