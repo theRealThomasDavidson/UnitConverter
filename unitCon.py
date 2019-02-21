07dd34ae3b4ff6eb59597d6f0c3a0a8f12061a50
thomas.davidson.18 <thomas.davidson.18@cnu.edu>
2019-02-21 00:29:30 -0500

    worked on some stuff not finished builidng an automatic tester to see if any units need prefix fixing

--- unitCon.py
+++ unitCon.py
@@ -16,15 +16,34 @@ def convert(unitFrom = None, unitTo = None):
         "Pa": 1., "bar": 100000., "at": 98066.5, "atm": 101325., "Torr": 133.322, "pfin": 6894.76
     }
     flowRateVol={
-        ("unit * L","unit /s"): 1., "cubftpmin": 2.11888, "cubmps": 0.001, "galps": 0.264172, "igillphr":25340.4
+        ("unit * L","unit / s"): 1., "cubftpmin": 2.11888, "cubmps": 0.001, "galps": 0.264172, "igillphr":25340.4
     }
     flowRateMass={
 
         "kgps": 1., "lbps": 0.453592, "gphr": (2.77778*10**(-7)),"gpm":(1.66667*10**(-5)), "gps": 0.001, "gpms":1.,
         "kgpms": 1000., "lbphr":12599.8,
     }
-    length={
-        "m": 1.,"in":.00254
+    length={"base":"unit * m",
+        "unit * m": 1./.00254,"unit * in":1.,"unit * ft":12.,"unit * yd":36.,"unit * mile":12.*5280.,"unit * ly":372469703644913410.,
+        "unit * au": 149597870700. / .00254
+    }
+    mass={"base":"unit * g",
+        "unit * g": 1.
+    }
+    time={"base":"unit * s",
+        "unit * s": 1.,"unit * min":60., "unit * hr":360.
+    }
+    elecCurr={"base":"unit * A",
+        "unit * A": 1.
+    }
+    temperature={"base":"unit * K",
+        "unit * K": 1.
+    }
+    luminous={"base":"unit * cd",
+        "unit * cd": 1.
+    }
+    mol={"base":"unit * mol",
+        "unit * mole": 1.
     }
 
 
@@ -34,20 +53,31 @@ def convert(unitFrom = None, unitTo = None):
     dimensions={
         "Pressure": pressure, "Volumetric Flow Rate":flowRateVol}#,"Mass Flow Rate": flowRateMass, "Length":length}
 
-    #TODO: handle dimensional analysis
+    baseUnits={"length":length, "mass":mass, "time":time, "Current":elecCurr, "temperature":temperature,
+               "intensity":luminous, "mole":mol }
+    #TODO: test how units in lists are handled.
+    for units in baseUnits:
+        for item in baseUnits[units]:
+            if item!="base":
+                #print("this is the item",item[6:],units).
+                #print(item)
+                #print(parseUnit(item[7:])[0][0])
+
+                if parseUnit(item[7:])[0][0]!= item:
+                    print item,parseUnit(item[7:])[0][0],"\n"
 
     uListFrom,scaleFrom=parseUnit(unitFrom)
     #uListTo=parseUnit(unitTo)
 
     uListFrom= tuple(uListFrom)
-    for dimension in dimensions:
-        print(dimension)
 
-        for item in dimensions[dimension]:
-            print(item,uListFrom)
-            if item==uListFrom:
-                print("hello")
 
+    """
+    for dimension in baseUnits:
+        print(uListFrom)
+        if uListFrom in dimensions[dimension]:
+            print("hello")
+    """
 
 
     return "Dimension type not available or are not printed right. Try using convert([name of dimension type]) " \
@@ -82,7 +112,7 @@ def parseUnit(toParse):
     for char in toParse:
 
         holdLength = len(retList)
-        if holdLength>0:
+        if holdLength > 0:
             if expBool:
                 if char=="-":
                     for ii in range(holdLength):
@@ -93,14 +123,16 @@ def parseUnit(toParse):
                             functPlace="/"
                             break
                     retList[-1]=retList[-1][:5]+functPlace+retList[-1][6:]
-                    if retList[-2][:4]=="pfix":
-                        retList[-2] = retList[-2][:5] + functPlace + retList[-2][6:]
+                    if holdLength>1:
+                        if retList[-2][:4]=="pfix":
+                            retList[-2] = retList[-2][:5] + functPlace + retList[-2][6:]
                 else:
                     repeatList=[]
                     number=int(char)
                     repeatList.append(retList[-1])
-                    if retList[-2][:4] == "pfix":
-                        repeatList.append(retList[-2][:5] + functPlace + retList[-2][6:])
+                    if holdLength>1:
+                        if retList[-2][:4] == "pfix":
+                            repeatList.append(retList[-2][:5] + functPlace + retList[-2][6:])
                     for ii in range (1,number):
                         for item in repeatList:
                             retList.append(item)
@@ -114,7 +146,7 @@ def parseUnit(toParse):
             retList.append("pfix "+ functPlace +" "+char)
             lookForPref=0
             if len(unit)!=0:
-                retList.append("unit "+unit)
+                retList.append("unit "+ functPlace +" "+unit)
                 unit=""
         elif char in functs:
             #print char, functs[char]
@@ -123,8 +155,7 @@ def parseUnit(toParse):
                 retList.append("unit "+ functPlace +" "+unit)
                 unit=""
             if retList[-1][:4]=="pfix":
-                print("hi", retList[-1])
-                temp="unit"+retList[-1][4:]
+                temp="unit "+retList[-1][4:]
                 del retList[-1]
                 retList.append(temp)
             if char != "^":
@@ -138,6 +169,10 @@ def parseUnit(toParse):
     if len(unit) != 0:
         retList.append("unit "+functPlace+" "+unit)
         unit = ""
+    if retList[-1][:4] == "pfix":
+        temp = "unit " + retList[-1][5:]
+        del retList[-1]
+        retList.append(temp)
     #by here the list is handled well but we need to
     retList=sorted(retList)
     for possPfix in retList:
@@ -148,14 +183,29 @@ def parseUnit(toParse):
             scale*=prefix[possPfix[7]]**exp
     retList=filter(lambda x: x[:4] != "pfix", retList)
     return (retList, scale)
-string="L/s"
 
 
-print(parseUnit("Mm^-1/in^4"))
-#convert("L/s")
+
+
+
+def main():
+    string = "dL/s"
+    #print(parseUnit("m^-1*thisisaunits*alsoaunit/notreqprefix/in^4"))
+    print convert(string)
+
+def testForprefixbreaks()
+if __name__=="__main__":
+    main()
+
+
 
 #print sorted( parseUnit(string)[0]), string
 
+"""
+testString="false: 0, true: 1"
+
+print(bool(int(testString[7])))
+"""
 """
        if char in prefix and lookForPref:
             if len(retList) > 0:
